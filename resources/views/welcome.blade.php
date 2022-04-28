16c3ddc49c851516cc09543381d418e4649ddd9a
YounessChakir03 <chakir.hidan@gmail.com>
2022-04-28 02:09:15 +0000

    filo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,7 +42,7 @@
           </div>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">1Disabled</a>
         </li>
       </ul>
     </div>
